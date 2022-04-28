feaa7725e51fd64c2290afd6da51cfc6637840aa
izhankhilji <unaisshazan@gmail.com>
2022-04-28 20:41:40 +0500

    Update Certificates.js

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -43,6 +43,28 @@ function Certificates() {
 							<h5>
 								{" "}
 								<span className="purple">
+								AWS Fundamentals: Migrating to the Cloud
+									<br />
+								</span>
+							</h5>
+							<strong>
+								<p>
+									ORGANIZATION :{" "}
+									<span className="purple">
+                                 AWS
+										<br />
+									</span>
+									DATE : <span className="purple">April 2022</span> <br />
+								</p>{" "}
+							</strong>
+							<Button href="https://coursera.org/share/d0310d399ce9c591a7f7b393835d75cd">
+								Click Here To View
+							</Button>
+						</Col>
+						<Col md={6} className="project-card" style={{ color: "white" }}>
+							<h5>
+								{" "}
+								<span className="purple">
                                 Crash Course on Python
 									<br />
 								</span>
